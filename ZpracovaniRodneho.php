cbe2acd95a8bb4af510891b1d83fbeeec81bb429
AzgorCZ <murdy159753@gmail.com>
2020-05-20 20:35:09 +0200

    převod na OOP vlastní funkce + vylepšení

--- ZpracovaniRodneho.php
+++ ZpracovaniRodneho.php
@@ -6,13 +6,18 @@
  * @author Vojta
  */
 class zpracovaniRodneho {
+    public $rodneCislo;
+    public function __construct($rodneCislo) {
+        $this->rodneCislo = $rodneCislo;
+    }
+    
     /**
      * 
      * @param type $rodneCislo
      * @return string
      */
-private function narozeni($rodneCislo){
-    $rok=substr($rodneCislo,0,2);
+private function narozeni(){
+    $rok=substr($this -> rodneCislo,0,2);
     $stoleti= substr(date("Y"),2,2);
                 if($rok <=$stoleti){ //pokud jsou první 2 čísla rodného čísla větší než poslední dvě čísla aktuálního století, tak osoba patří do roku 2000-Současný rok
                 $rok= "20".$rok;
@@ -28,8 +33,8 @@ private function narozeni($rodneCislo){
   * @param string $rodneCislo
   * @return int
   */
-       public function vek (string $rodneCislo){
-            $rok= $this -> narozeni($rodneCislo);
+       public function vek (){
+            $rok= $this -> narozeni();
             $vek=date("Y")-$rok;
             echo "<br>".$vek;
             return 0;
@@ -40,10 +45,10 @@ private function narozeni($rodneCislo){
          * @param string $rodneCislo
          * @return int
          */
-        public function datum (string $rodneCislo){
-      $rok= $this -> narozeni($rodneCislo);
-            $mesic = substr($rodneCislo,2,2);
-            $den = substr($rodneCislo,4,2);
+        public function datum (){
+      $rok= $this -> narozeni();
+            $mesic = substr($this -> rodneCislo,2,2);
+            $den = substr($this -> rodneCislo,4,2);
             if($mesic >50){
                 $mesic =$mesic - 50;
                 if($mesic <10){
@@ -59,9 +64,9 @@ private function narozeni($rodneCislo){
           return 0;
 }
 
-public function pohlavi (string $rodneCislo){
+public function pohlavi (){
     $pohlavi = "";
-    $mesic = substr($rodneCislo,2,2);
+    $mesic = substr($this -> rodneCislo,2,2);
     if ($mesic >50){
       $pohlavi= "žena";
       echo "<br>".$pohlavi;
@@ -75,5 +80,6 @@ public function pohlavi (string $rodneCislo){
     echo "<br>".$pohlavi;
     return 0;
 }
+
 
 }
--- index.php
+++ index.php
@@ -3,15 +3,14 @@ require 'C:/wamp64/www/tracy/tracy.phar';
 require_once('zpracovaniRodneho.php');
 
 use Tracy\Debugger;
-
 Debugger::enable();
 Debugger::$strictMode = true;
 
 $rodneCislo = "036027/1111";
 echo "rodne cislo: " . $rodneCislo;
-$var = new zpracovaniRodneho();
-$var->vek($rodneCislo);
-$var->datum($rodneCislo);
-$var ->pohlavi($rodneCislo);
+$var = new zpracovaniRodneho($rodneCislo);
+$var->vek();
+$var->datum();
+$var ->pohlavi();
 
 ?>
